6583a681127aa0da0f7280790e4ed03df18d1682
Alf Gaida <agaida@siduction.org>
2017-01-21 17:25:23 +0100

    * transfer home implemented   * file removal in transfered home implemented

--- modules/fllfixhome/main.py
+++ modules/fllfixhome/main.py
@@ -32,119 +32,149 @@ def run():
 
     # set root_mount_point
     root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
+    user = libcalamares.globalstorage.value("username")
+    # local INSTHOME="/home/${USER_NAME}"
+    insthome = os.path.join( root_mount_point, "home", user)
+    # home of user on live media
+    # local LIVEHOME="/home/${DEFAULT_USER}" --> /home/siducer
+    livehome = os.path.join( root_mount_point, "/home/siducer")
 
+    # move livehome --> insthome
+    #        if [ -d "${TARGET_MNT_POINT}${LIVEHOME}" ] && \
+    #           [ ! -d "${TARGET_MNT_POINT}${INSTHOME}" ]; then
+    #            mv "${TARGET_MNT_POINT}${LIVEHOME}" "${TARGET_MNT_POINT}${INSTHOME}"
+    shutil.move( livehome, insthome )¶
 
-#        local INSTHOME="/home/${USER_NAME}"
-#        # home of user on live media
-#        local LIVEHOME="/home/${DEFAULT_USER}"
-#
-#        if [ "${USER_NAME}" != "${DEFAULT_USER}" ];     then
-#                if [ -d "${TARGET_MNT_POINT}${LIVEHOME}" ] && \
-#                        [ ! -d "${TARGET_MNT_POINT}${INSTHOME}" ]; then
-#                        mv "${TARGET_MNT_POINT}${LIVEHOME}" "${TARGET_MNT_POINT}${INSTHOME}"
-#
-#                        # fix /home/user paths in various config files
-#                        find "${TARGET_MNT_POINT}${INSTHOME}" \
-#                                -type f \
-#                                -exec sed -i "s|${LIVEHOME}|${INSTHOME}|g" {} \;
-#                fi
-#        fi
+    # fix /home/user paths in various config files
+    #       find "${TARGET_MNT_POINT}${INSTHOME}" \
+    #          -type f \
+    #          -exec sed -i "s|${LIVEHOME}|${INSTHOME}|g" {} \;
 
-        # purge unwanted files
-#        for file in     "${TARGET_MNT_POINT}${INSTHOME}/Desktop/${FLL_DISTRO_NAME}.desktop" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/Desktop/install-gui.desktop" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.config/autostart/${FLL_DISTRO_NAME}.desktop" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.hushlogin" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.config/chromium/SingletonLock" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.config/chromium/Local State" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.config/chromium/First Run" \
-#                        "${TARGET_MNT_POINT}/root/.hushlogin"; do
-#                rm -f "${file}"
-#        done
+    # purge unwanted files
+    # ${TARGET_MNT_POINT}${INSTHOME}/Desktop/${FLL_DISTRO_NAME}.desktop
+    unwanted = os.path.join( insthome, "/Destktop/siduction.desktop" )
+    os.remove( unwanted )
 
-        # purge content of some dirs
-#        for dir in "${TARGET_MNT_POINT}${INSTHOME}/.cache"; do
-#            [ -d "$dir" ] && rm -Rf ${dir}/*
-#        done
+    # ${TARGET_MNT_POINT}${INSTHOME}/Desktop/install-gui.desktop
+    unwanted = os.path.join( insthome, "/Destktop/install-gui.desktop" )
+    os.remove( unwanted )
 
-        # revert sudo workarounds
-#        for file in     "${TARGET_MNT_POINT}${INSTHOME}/.config/kdesurc" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.kde/share/config/kdesurc" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.kde/share/apps/konsole/sumc.desktop" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.kde/share/apps/konsole/su.desktop" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.gconf/apps/gksu/%gconf.xml" \
-#                        "${TARGET_MNT_POINT}${INSTHOME}/.su-to-rootrc"; do
-#                grep -s -q sudo "$file" && rm -f "$file"
-#        done
+    # ${TARGET_MNT_POINT}${INSTHOME}/.config/autostart/${FLL_DISTRO_NAME}.desktop
+    unwanted = os.path.join( insthome, ".config/autostart/siduction.desktop" )
+    os.remove( unwanted )
 
+    # ${TARGET_MNT_POINT}${INSTHOME}/.hushlogin
+    unwanted = os.path.join( insthome, ".hushlogin" )
+    os.remove( unwanted )
 
-        # revert gksu sudo mode hack
-#        logit "revert gksu sudo mode hack"
-#        chroot "${TARGET_MNT_POINT}" \
-#                sudo -u "${FLL_LIVE_USER}" gconftool-2 -s -t bool /apps/gksu/sudo-mode false
-#        chroot "${TARGET_MNT_POINT}" \
-#                sudo -u "${FLL_LIVE_USER}" gconftool-2 -s -t bool /apps/gksu/display-no-pass-info true
+    # ${TARGET_MNT_POINT}${INSTHOME}/.config/chromium/SingletonLock
+    unwanted = os.path.join( insthome, ".config/chromium/SingletonLock" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.config/chromium/Local State
+    unwanted = os.path.join( insthome, ".config/chromium/Local State" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.config/chromium/First Run
+    unwanted = os.path.join( insthome, ".config/chromium/First Run" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}/root/.hushlogin
+    unwanted = os.path.join( root_mount_point, "root/.hushlogin" )
+    os.remove( unwanted )
+
+    # purge content of some dirs
+    #        for dir in "${TARGET_MNT_POINT}${INSTHOME}/.cache"; do
+    #            [ -d "$dir" ] && rm -Rf ${dir}/*
+    #        done
+
+    # revert sudo workarounds
+    # don't test for now grep -s -q sudo "$file" && rm -f "$file"
+    # ${TARGET_MNT_POINT}${INSTHOME}/.config/kdesurc
+    unwanted = os.path.join( insthome, ".config/kdesurc" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.kde/share/config/kdesurc
+    unwanted = os.path.join( insthome, ".kde/share/config/kdesurc" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.kde/share/apps/konsole/sumc.desktop
+    unwanted = os.path.join( insthome, ".kde/share/apps/konsole/sumc.desktop" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.kde/share/apps/konsole/su.desktop
+    unwanted = os.path.join( insthome, ".kde/share/apps/konsole/su.desktop" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.gconf/apps/gksu/%gconf.xml
+    unwanted = os.path.join( insthome, ".gconf/apps/gksu/%gconf.xml" )
+    os.remove( unwanted )
+
+    # ${TARGET_MNT_POINT}${INSTHOME}/.su-to-rootrc
+    unwanted = os.path.join( insthome, ".su-to-rootrc" )
+    os.remove( unwanted )
+
+    # revert gksu sudo mode hack
+    # sudo -u "${FLL_LIVE_USER}" gconftool-2 -s -t bool /apps/gksu/sudo-mode false
+    # sudo -u "${FLL_LIVE_USER}" gconftool-2 -s -t bool /apps/gksu/display-no-pass-info true
 
     # remove installer from fluxbox menu
-#        if grep -s -q Installer "${TARGET_MNT_POINT}${INSTHOME}"/.fluxbox/fll-flux-*; then
-#                sed -i '/Installer/d' "${TARGET_MNT_POINT}${INSTHOME}"/.fluxbox/fll-flux-*
-#        fi
+    # if grep -s -q Installer "${TARGET_MNT_POINT}${INSTHOME}"/.fluxbox/fll-flux-*; then
+    #    sed -i '/Installer/d' "${TARGET_MNT_POINT}${INSTHOME}"/.fluxbox/fll-flux-*
+    # fi
 
     # make sudo alias's vanish
-#        if grep -s -q sudo "${TARGET_MNT_POINT}${INSTHOME}/.bashrc"; then
-#                sed -i 's|\(.*sudo.*\)||' "${TARGET_MNT_POINT}${INSTHOME}/.bashrc"
-#        fi
+    # if grep -s -q sudo "${TARGET_MNT_POINT}${INSTHOME}/.bashrc"; then
+    #    sed -i 's|\(.*sudo.*\)||' "${TARGET_MNT_POINT}${INSTHOME}/.bashrc"
+    # fi
 
     # enable automount-open for gnome, mate and cinnamon in install mode
-#        if [ -d /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE} ]; then
-#                case "$FLL_FLAVOUR" in
-#                        gnome)
-#                                cat > "${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open" <<EOF
+    # if [ -d /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE} ]; then
+    #   case "$FLL_FLAVOUR" in
+    #                        gnome)
+    #                                cat > "${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open" <<EOF
 
-# #!/bin/sh
-# if [ ! -d /usr/share/fll-installer ]; then
-#    gsettings set org.gnome.desktop.media-handling automount true
-#    gsettings set org.gnome.desktop.media-handling automount-open true
-#    rm ${INSTHOME}/.config/autostart/automount-open.desktop
-# fi
-# EOF
-#                        ;;
-#                        cinnamon)
-#                                cat > "${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open" <<EOF
-# #!/bin/sh
-# if [ ! -d /usr/share/fll-installer ]; then
-#     gsettings set org.cinnamon.desktop.media-handling automount true
-#     gsettings set org.cinnamon.desktop.media-handling automount-open true
-#     rm ${INSTHOME}/.config/autostart/automount-open.desktop
-# fi
-# EOF
-#                                 ;;
-#                         mate)
-#                                 cat > "${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open" <<EOF
-# #!/bin/sh
-# if [ ! -d /usr/share/fll-installer ]; then
-#     gsettings set org.mate.media-handling automount true
-#     gsettings set org.mate.media-handling automount-open true
-#     rm ${INSTHOME}/.config/autostart/automount-open.desktop
-# fi
-# EOF
-#                                 ;;
-#         esac
+                                   # if [ ! -d /usr/share/fll-installer ]; then
+                                 #    gsettings set org.gnome.desktop.media-handling automount true
+                                 #    gsettings set org.gnome.desktop.media-handling automount-open true
+                                 #    rm ${INSTHOME}/.config/autostart/automount-open.desktop
 
-#         # We don't check existence, -p handles that
-#         mkdir -p ${TARGET_MNT_POINT}${INSTHOME}/.config/autostart
+                              # EOF
+    #                        ;;
+    #                        cinnamon)
+    #                                cat > "${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open" <<EOF
+                                 # #!/bin/sh
+                                    # if [ ! -d /usr/share/fll-installer ]; then
+                                             #     gsettings set org.cinnamon.desktop.media-handling automount true
+                                            #     gsettings set org.cinnamon.desktop.media-handling automount-open true
+                                           #     rm ${INSTHOME}/.config/autostart/automount-open.desktop
+                                     # fi
+                                               # EOF
+    #                                 ;;
+    #                         mate)
+    #                                 cat > "${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open" <<EOF
+                             # #!/bin/sh
+                               # if [ ! -d /usr/share/fll-installer ]; then
+                               #     gsettings set org.mate.media-handling automount true
+                               #     gsettings set org.mate.media-handling automount-open true
+                                 #     rm ${INSTHOME}/.config/autostart/automount-open.desktop
+                               # fi
+                                # EOF
+                             #                                 ;;
+                               #         esac
+
+    # We don't check existence, -p handles that
+    # mkdir -p ${TARGET_MNT_POINT}${INSTHOME}/.config/autostart
 
     # fix permisions for automount-open automount-open.desktop and /.config/autostart
-#        chroot "${TARGET_MNT_POINT}" \
-#        chown ${USER_NAME} ${INSTHOME}/.config/autostart
-#        chroot "${TARGET_MNT_POINT}" \
-#        chgrp ${USER_NAME} ${INSTHOME}/.config/autostart
-#        chmod +x ${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open
-#        chroot "${TARGET_MNT_POINT}" \
-#        ln -sf /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open.desktop ${INSTHOME}/.config/autostart/automount-open.desktop
-#        chroot "${TARGET_MNT_POINT}" \
-#        chown ${USER_NAME} /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open
-#        chroot "${TARGET_MNT_POINT}" \
-#        chown ${USER_NAME} /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open.desktop
-#        fi
-#}
+    # chown ${USER_NAME} ${INSTHOME}/.config/autostart
+
+    # chgrp ${USER_NAME} ${INSTHOME}/.config/autostart
+
+    # chmod +x ${TARGET_MNT_POINT}/usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open
+
+    # ln -sf /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open.desktop ${INSTHOME}/.config/autostart/automount-open.desktop
+
+    # chown ${USER_NAME} /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open
+
+    # chown ${USER_NAME} /usr/share/siduction-settings-${FLL_FLAVOUR}-${FLL_DISTRO_CODENAME_SAFE}/automount-open.desktop
